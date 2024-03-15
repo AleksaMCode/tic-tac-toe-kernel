5485fe5f3f02379989c69441d224431643d9eadd
aleksamcode <aleksamcode@gmail.com>
2024-03-15 17:30:06 +0100

    Set Google format style

--- formatter.sh
+++ formatter.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # The cool idea for formatting is from: https://stackoverflow.com/a/36046965/9917714
-find src/ -iname *.h -o -iname *.c | xargs clang-format -i
+find src/ -iname *.h -o -iname *.c | xargs clang-format --style=Google -i
